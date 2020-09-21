421290aeb2291beec12472b8e31db6e402f2f2ef
02masaki <69668107+02masaki@users.noreply.github.com>
2020-09-21 11:30:37 +0900

    購入機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,35 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(address_params)
+    if @order_address.valid? 
+      @order_address.save
+      pay_item
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency:'jpy'
+    )
+ end
+
+ def address_params
+  params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :tel).merge(token: params[:token], item_id: @item.id, user_id: current_user.id)
+ end
+
+end
---
+++ app/forms/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+
+  include ActiveModel::Model
+  attr_accessor :prefecture_id, :city, :address, :building, :tel, :postal_code, :token, :item_id, :user_id
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1}
+    validates :city, format: {with: /\A[一-龥ぁ-ん]/}
+    validates :address
+    validates :tel, format: {with: /\A\d{10,11}\z/}
+    validates :token
+  end
+
+  def save
+    Order.create(item_id: item_id, user_id: user_id )
+    Address.create(prefecture_id: prefecture_id, city: city, address: address, building: building, postal_code: postal_code, tel: tel, item_id: item_id)
+  end
+
+end
