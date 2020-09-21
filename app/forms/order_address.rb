9a724009ed3fef234ab17774808414914a1d96e2
02masaki <69668107+02masaki@users.noreply.github.com>
2020-09-21 19:19:09 +0900

    formオブジェクトの編集２

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id )
-    Address.create(prefecture_id: prefecture_id, city: city, address: address, building: building, postal_code: postal_code, tel: tel, order_id: order_id)
+    Address.create(prefecture_id: prefecture_id, city: city, address: address, building: building, postal_code: postal_code, tel: tel, order_id: order.id)
   end
 
 end
